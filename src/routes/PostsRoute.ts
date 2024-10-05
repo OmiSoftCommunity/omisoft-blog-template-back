65af00add5dcdbc5ed0e4623f8fa652824c9dc52
Yevhen S <eresid@gmail.com>
2024-10-05 15:11:54 +0200

    fixed API restrictions

--- src/routes/PostsRoute.ts
+++ src/routes/PostsRoute.ts
@@ -14,21 +14,22 @@ import {
 } from "../controllers/posts";
 import multer from "multer";
 import { adminVerifyMiddleware } from "../middleware/RoleVerifyMiddleware";
+import authVerifyMiddleware from "../middleware/AuthVerifyMiddleware";
 const upload = multer({ dest: "uploads/" });
 
 export default () => {
   const route = Router();
 
-  route.post(API_ROUTES.POSTS.CREATE, CreatePostController);
-  route.delete(API_ROUTES.POSTS.DELETE, adminVerifyMiddleware, DeletePostController);
-  route.get(API_ROUTES.POSTS.ALL_POSTS_ADMIN, GetAllPostAdminController);
+  route.post(API_ROUTES.POSTS.CREATE, authVerifyMiddleware, CreatePostController);
+  route.delete(API_ROUTES.POSTS.DELETE, [authVerifyMiddleware, adminVerifyMiddleware], DeletePostController);
+  route.get(API_ROUTES.POSTS.ALL_POSTS_ADMIN, authVerifyMiddleware, GetAllPostAdminController);
   route.get(API_ROUTES.POSTS.ALL_POSTS, GetAllPostsController);
   route.get(API_ROUTES.POSTS.POSTS_URL, GetAllPostsUrlController);
   route.get(API_ROUTES.POSTS.POST_BY_ID, GetPostByIdController);
   route.get(API_ROUTES.POSTS.POST_BY_URL, GetPostByUrlController);
-  route.get(API_ROUTES.POSTS.BACKUP, PostsBackupController);
-  route.post(API_ROUTES.POSTS.IMPORT, upload.single("posts"), ImportPostsController);
-  route.put(API_ROUTES.POSTS.UPDATE_POST, UpdatePostByIdController);
+  route.get(API_ROUTES.POSTS.BACKUP, authVerifyMiddleware, PostsBackupController);
+  route.post(API_ROUTES.POSTS.IMPORT, [authVerifyMiddleware, upload.single("posts")], ImportPostsController);
+  route.put(API_ROUTES.POSTS.UPDATE_POST, authVerifyMiddleware, UpdatePostByIdController);
 
   return route;
 };
--- src/routes/UsersRoute.ts
+++ src/routes/UsersRoute.ts
@@ -18,16 +18,24 @@ import authVerifyMiddleware from "../middleware/AuthVerifyMiddleware";
 export default () => {
   const route = Router();
 
-  route.put(API_ROUTES.USERS.CLEAR_PASSWORD, adminVerifyMiddleware, ClearPasswordController);
-  route.post(API_ROUTES.USERS.CREATE_MODERATOR, adminVerifyMiddleware, CreateModeratorController);
-  route.post(API_ROUTES.USERS.CREATE, CreateUserController);
-  route.delete(API_ROUTES.USERS.DELETE, DeleteUserController);
-  route.delete(API_ROUTES.USERS.DELETE_FOR_ADMIN, adminVerifyMiddleware, DeleteUserForAdminController);
-  route.get(API_ROUTES.USERS.GET_ADMIN, GetAdminController);
-  route.get(API_ROUTES.USERS.ALL_USERS, GetAllUsersController);
+  route.put(API_ROUTES.USERS.CLEAR_PASSWORD, [authVerifyMiddleware, adminVerifyMiddleware], ClearPasswordController);
+  route.post(
+    API_ROUTES.USERS.CREATE_MODERATOR,
+    [authVerifyMiddleware, adminVerifyMiddleware],
+    CreateModeratorController
+  );
+  route.post(API_ROUTES.USERS.CREATE, authVerifyMiddleware, CreateUserController);
+  route.delete(API_ROUTES.USERS.DELETE, authVerifyMiddleware, DeleteUserController);
+  route.delete(
+    API_ROUTES.USERS.DELETE_FOR_ADMIN,
+    [authVerifyMiddleware, adminVerifyMiddleware],
+    DeleteUserForAdminController
+  );
+  route.get(API_ROUTES.USERS.GET_ADMIN, authVerifyMiddleware, GetAdminController);
+  route.get(API_ROUTES.USERS.ALL_USERS, authVerifyMiddleware, GetAllUsersController);
   route.get(API_ROUTES.USERS.GET_MY_PROFILE, authVerifyMiddleware, GetMyProfileController);
-  route.get(API_ROUTES.USERS.USER_BY_ID, GetUserByIdController);
-  route.put(API_ROUTES.USERS.UPDATE_USER, UpdateUserController);
+  route.get(API_ROUTES.USERS.USER_BY_ID, authVerifyMiddleware, GetUserByIdController);
+  route.put(API_ROUTES.USERS.UPDATE_USER, authVerifyMiddleware, UpdateUserController);
 
   return route;
 };
